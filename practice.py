da76059282ae86424dc2fcbe1560d4935106953f
Emilymatoke <emilymomata@gmail.com>
2024-03-11 07:58:22 +0300

    Completed

---
+++ practice.py
@@ -0,0 +1,19 @@
+class Dog :
+    pass
+fido = Dog()
+fido
+
+# <__main__.Dog object at 0x1049a87f0>
+
+Snoopy = Dog()
+Snoopy
+
+# <__main__.Dog object at 0x104971d90>
+
+lassie = Dog()
+lassie
+
+# <__main__.Dog object at 0x10498c040>
+
+Snoopy == fido
+# False
